0fb64390cd9f14b33b665389d728abea05b3e131
Alonso <asorianom@iesch.org>
2025-11-04 01:36:33 +0100

    Lo que sea esto

--- Java/Basicos/Prueba1/pelota.java
+++ Java/Basicos/Prueba1/pelota.java
@@ -1,7 +1,30 @@
 package Java.Basicos.Prueba1;
 
 public class pelota {
-    int posy;
-    int posx;
-    int[] velocidad = {,};
+    int posiciony;
+    int posicionx;
+    int[] velocidad = {0,0};
+    //Compruebo que la velocidad es posible
+    public static int[] compVel(int posx, int posy, int[] vel){
+        if (posx + vel[0] > 9){
+            vel[0] = -1;
+        }else if (posx + vel[0] < 0 ){
+            vel[0] = 1;
+        }
+        if (posy + vel[1] > 4){
+            vel[1] = -1;
+        }else if (posy + vel[1] < 0 ){
+            vel[1] = 1;
+        }
+        return vel;
+    }
+    //Mueve la pelota sumandole o restandole a su eje y e x dependiendo de su velocidad
+    public static int moverx(int pos, int[] vel){
+        pos += vel[0];
+        return pos;
+    }
+    public static int movery(int pos, int[] vel){
+        pos += vel[1];
+        return pos;
+    }
 }
--- Java/Basicos/Prueba1/programa.java
+++ Java/Basicos/Prueba1/programa.java
@@ -2,24 +2,26 @@ package Java.Basicos.Prueba1;
 import java.util.*;
 
 public class programa {
-    pelota bola = new pelota();
-    void mover(){
-
-    }
-    public static void impTabla(){
-        int cont = 0;
+    //Esta función imprime la tabla con la pelota, primero descarto la primera y la ultima fila y luego pares e impares
+    public static void impTabla(String[][] tabl){
+        int c = 0;
         for (int i = 0; i <= 10; i++) {
+            int b = -1;
             for (int j = 0; j <= 20; j++) {
+                //Primera fila
                 if (i == 0){
                     if (j == 0){
-                        System.out.print("╔");
+                        System.out.print("\n╔");
                     } else if (j == 20){
                         System.out.print("╗");
                     } else if (j%2 == 0){
                         System.out.print("╦");
                     } else {
-                        System.out.print("═");
+                        for (int a = 0; a < 3; a++){
+                            System.out.print("═");
+                        }
                     }
+                    //Ultima fila
                 } else if (i == 10){
                     if (j == 0){
                         System.out.print("╚");
@@ -28,37 +30,54 @@ public class programa {
                     } else if (j%2 == 0){
                         System.out.print("╩");
                     } else {
-                        System.out.print("═");
+                        for (int a = 0; a < 3; a++){
+                            System.out.print("═");
+                        }
                     }
+                    //Pares
                 } else if (i%2 == 0) {
                     if (j == 0){
                         System.out.print("╠");
+                        c++;
                     } else if (j == 20){
                         System.out.print("╣");
                     } else if (j%2 == 0){
                         System.out.print("╬");
                     } else {
-                        System.out.print("═");
+                        for (int a = 0; a < 3; a++){
+                            System.out.print("═");
+                        }
                     }
+                    //Impares
                 } else {
                     if (j%2 == 0){
                         System.out.print("║");
                     } else {
-                        System.out.print(" ");
+                        b++;
+                        System.out.print(" " + tabl[c][b] + " ");
                     }
                 }
             }
-            System.out.println("");
+            if (i != 10) {
+                System.out.println();
+            }
         }
     }
 
-    static void main() {
+    static void main() throws InterruptedException {
+        pelota bola = new pelota();
         Scanner sc = new Scanner(System.in);
         String[][] tabla = {{" "," "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "," "},{" "," "," "," "," "," "," "," "," "," "}};
         String op;
+        int cont = 0, pos = 0, tiempo;
+        Double vel = 0.0;
+        boolean valido = false;
+
+        //Aquí imprimo la tabla con las posiciones, primero descarto la primera y la ultima fila y luego pares e impares
         for (int i = 0; i <= 10; i++) {
             for (int j = 0; j <= 20; j++) {
                 if (i == 0){
+                    //Primera fila
                     if (j == 0){
                         System.out.print("╔");
                     } else if (j == 20){
@@ -66,8 +85,11 @@ public class programa {
                     } else if (j%2 == 0){
                         System.out.print("╦");
                     } else {
-                        System.out.print("═");
+                        for (int a = 0; a < 3; a++){
+                            System.out.print("═");
+                        }
                     }
+                    //Ultima fila
                 } else if (i == 10){
                     if (j == 0){
                         System.out.print("╚");
@@ -76,8 +98,11 @@ public class programa {
                     } else if (j%2 == 0){
                         System.out.print("╩");
                     } else {
-                        System.out.print("═");
+                        for (int a = 0; a < 3; a++){
+                            System.out.print("═");
+                        }
                     }
+                    //Filas pares
                 } else if (i%2 == 0) {
                     if (j == 0){
                         System.out.print("╠");
@@ -86,25 +111,112 @@ public class programa {
                     } else if (j%2 == 0){
                         System.out.print("╬");
                     } else {
-                        System.out.print("═");
+                        for (int a = 0; a < 3; a++){
+                            System.out.print("═");
+                        }
                     }
+                    //Filas impares
                 } else {
                     if (j%2 == 0){
                         System.out.print("║");
                     } else {
-                        System.out.print(" ");
+                        cont++;
+                        if (cont < 10) {
+                            System.out.print(cont + "  ");
+                        } else {
+                            System.out.print(cont + " ");
+                        }
                     }
                 }
             }
-            System.out.println("");
+            System.out.println();
         }
+
+        //Pido los valores posición y dirección
         do {
-            System.out.print("Hacia donde quieres que se mueva la pelota\n> ");
+            System.out.print("¿En que posición quieres que empiece la pelota?\n> ");
+            try {
+                pos = Integer.parseInt(sc.nextLine());
+                if (pos > 50){
+                    System.out.println("Te has pasado");
+                } else if (pos <= 0){
+                    System.out.println("Posición incorrecta");
+                } else {valido = true;}
+            } catch (NumberFormatException e){
+                System.out.println("Formato invalido");
+            }
+        } while (!valido);
+
+        //Calculo la posición en el array
+        bola.posicionx = (pos-1) % 10 ;
+        bola.posiciony = (pos-1) / 10;
+        tabla[bola.posiciony][bola.posicionx] = "O";
+        impTabla(tabla);
+
+        //Determino hacia donde se va a mover la bola
+        do {
+            System.out.print("\n----------------------------\n¿Hacia donde quieres que se mueva la pelota?\n----------------------------\nArriba derecha\nArriba izquierda\nAbajo derecha\nAbajo izquierda\n----------------------------\n> ");
             op =  sc.nextLine();
-            if (!op.equals("arriba derecha") && !op.equals("abajo derecha") && !op.equals("abajo izquierda") && !op.equals("abajo izquierda")){
+            if (!op.equals("arriba derecha") && !op.equals("abajo derecha") && !op.equals("arriba izquierda") && !op.equals("abajo izquierda") && !op.equals("Arriba derecha") && !op.equals("Abajo derecha") && !op.equals("Arriba izquierda") && !op.equals("Abajo izquierda")){
                 System.out.println("Dirección invalida");
             }
-        }while (!op.equals("arriba derecha") && !op.equals("abajo derecha") && !op.equals("abajo izquierda") && !op.equals("abajo izquierda"));
+        }while (!op.equals("arriba derecha") && !op.equals("abajo derecha") && !op.equals("arriba izquierda") && !op.equals("abajo izquierda") && !op.equals("Arriba derecha") && !op.equals("Abajo derecha") && !op.equals("Arriba izquierda") && !op.equals("Abajo izquierda"));
+        switch (op){
+            case "arriba derecha", "Arriba derecha":
+                bola.velocidad[0] = 1;
+                bola.velocidad[1] = -1;
+                break;
+            case "arriba izquierda", "Arriba izquierda":
+                bola.velocidad[0] = -1;
+                bola.velocidad[1] = -1;
+                break;
 
+            case "abajo derecha", "Abajo derecha":
+                bola.velocidad[0] = 1;
+                bola.velocidad[1] = 1;
+                break;
+            case "abajo izquierda", "Abajo izquierda":
+                bola.velocidad[0] = -1;
+                bola.velocidad[1] = 1;
+                break;
+        }
+
+        //Con que velocidad...
+        valido = false;
+        do {
+            System.out.print("¿Con que velocidad quieres que se mueva? (5-Recomendado)\n> ");
+            try {
+                vel = Double.parseDouble(sc.nextLine());
+                if (vel < 0){
+                    System.out.println("Velocidad incorrecta");
+                } else {valido = true;}
+            } catch (NumberFormatException e){
+                System.out.println("Formato invalido");
+            }
+        } while (!valido);
+        System.out.println(vel);
+        tiempo = (int) (3000 / vel);
+        while (true){
+            System.out.println("\n\n\n\n\n\n\n");
+            impTabla(tabla);
+            for (int i = 0; i < 5; i++){
+                for (int j = 0; j < 10; j++){
+                    if (tabla[i][j] == "."){
+                        tabla[i][j] = " ";
+                    }
+                    if (tabla[i][j] == "o"){
+                        tabla[i][j] = ".";
+                    }
+
+                }
+            }
+            tabla[bola.posiciony][bola.posicionx] = "o";
+            bola.velocidad = bola.compVel(bola.posicionx, bola.posiciony, bola.velocidad);
+            bola.posicionx = bola.moverx(bola.posicionx, bola.velocidad);
+            bola.posiciony = bola.movery(bola.posiciony, bola.velocidad);
+            tabla[bola.posiciony][bola.posicionx] = "O";
+            Thread.sleep(tiempo);
+
+        }
     }
 }
