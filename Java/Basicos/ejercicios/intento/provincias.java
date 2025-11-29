5ca040b9ab65c1ca2a4ac69a42cd9ac88a68bb41
Alonso <alonsoriano2007@gmail.com>
2025-11-29 18:42:38 +0100

    pueblos descubrimiento

---
+++ Java/Basicos/ejercicios/intento/provincias.java
@@ -0,0 +1,12 @@
+package Java.Basicos.ejercicios.intento;
+
+import java.util.ArrayList;
+
+public class provincias {
+    ArrayList <String> pueblos = new ArrayList<String>();
+    String nombre;
+
+    public provincias(String nombre) {
+        this.nombre = nombre;
+    }
+}
