3902617e77141e120fcca7295662575263842d55
Alonso <alonsoriano2007@gmail.com>
2025-11-30 19:13:59 +0100

    pueblos TERMINADO

--- Java/Basicos/ejercicios/intento/ej22Manera2.java
+++ Java/Basicos/ejercicios/intento/ej22Manera2.java
@@ -1,12 +1,12 @@
 package Java.Basicos.ejercicios.intento;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ej22Manera2 {
     //Creo el array list
-    static ArrayList<provincias> provinciasArray = new ArrayList<provincias>();
+    static ArrayList<provincias> provinciasArray = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
     static void main() {
         //Añado varias provincias
@@ -15,38 +15,62 @@ public class ej22Manera2 {
         provinciasArray.add(new provincias("Teruel"));
         // Y pueblos
         provinciasArray.get(0).pueblos.add("pueblo1");
-        provinciasArray.get(0).pueblos.add("pueblo2");
-        provinciasArray.get(0).pueblos.add("pueblo3");
         provinciasArray.get(0).pueblos.add("pueblo4");
+        provinciasArray.get(0).pueblos.add("pueblo3");
+        provinciasArray.get(0).pueblos.add("pueblo2");
         provinciasArray.get(0).pueblos.add("pueblo5");
         provinciasArray.get(0).pueblos.add("pueblo6");
 
         provinciasArray.get(1).pueblos.add("pueblo1");
         provinciasArray.get(1).pueblos.add("pueblo2");
+        do {
 
-
-        //muestro de forma numerada las provincias para el menú
-        System.out.println("¿Con que provincia quieres trabajar?");
-        mostrarProvincias();
-        System.out.print("\n| s - Salir\n| m - Mostrar todos los pueblos\n> ");
-        String op = sc.nextLine();
-        op = op.toLowerCase();
-        if (op.equals("s")){
-            return;
-        } else if (op.equals("m")) {
-            mostrarPueblos();
-        } else {
-            if (transformarAEntero(op) == -1){
-                System.out.println("ERROR - Carácter invalido");
+            //muestro de forma numerada las provincias para el menú
+            System.out.println("<=====================================================>");
+            System.out.println("¿Con que provincia quieres trabajar?");
+            mostrarProvincias();
+            System.out.print("\n| e - Eliminar provincia\n| m - Mostrar todos los pueblos\n| a - Añadir provincia\n| s - Salir\n└> ");
+            String op = sc.nextLine();
+            //Compruebo si la opción es una letra que sea una opción
+            if (op.equalsIgnoreCase("s")){
+                return;
+            } else if (op.equalsIgnoreCase("m")) {
+                mostrarPueblos();
+            } else if (op.equalsIgnoreCase("a")) {
+                anadirProvincia();
+            } else if (op.equalsIgnoreCase("e")) {
+                eliminarProvincia();
             } else {
-                System.out.println(transformarAEntero(op));
-                System.out.println("PROGRAMA AQUI");
+                //si es un numero compruebo que sea una opción valida
+                int pos = transformarAEntero(op);
+                if (pos == -11 && op.length()!=3){
+                    System.out.println("ERROR - Opción invalida");
+                } else if (pos >= provinciasArray.size() || pos < 0) {
+                    System.out.println("ERROR - Posición invalida");
+                } else {
+                    do {
+                        System.out.println("<------------------------------------------------------>");
+                        System.out.print("¿Que quieres hacer con " + provinciasArray.get(pos).nombre + "?\n| 1 - Mostrar pueblos\n| 2 - Buscar pueblos\n| 3 - Aañadir pueblos\n| 4 - Borrar pueblos\n| s - Salir\n└> ");
+                        String accion = sc.nextLine();
+                        if (accion.equalsIgnoreCase("s")){
+                            break;
+                        } else if (accion.equals("1")){
+                            mostrarPueblosDeProvincia(provinciasArray.get(pos).pueblos, 1);
+                        } else if (accion.equals("2")) {
+                            buscarPueblo(provinciasArray.get(pos).pueblos);
+                        } else if (accion.equals("3")) {
+                            anadirPueblo(provinciasArray.get(pos).pueblos);
+                        } else if (accion.equals("4")) {
+                            eliminarPueblo(provinciasArray.get(pos).pueblos);
+                        } else {
+                            System.out.println("ERROR - Opción invalida");
+                        }
+                    } while (true);
+                }
             }
-        }
-
-
-
+        } while (true);
     }
+
     static void mostrarProvincias(){
         //Cada 30 caracteres se salta de línea
         int cont = 0;
@@ -62,8 +86,10 @@ public class ej22Manera2 {
     }
 
     static void mostrarPueblos(){
+        System.out.println();
         //Muestro bonito las provincias
         for (provincias i : provinciasArray){
+            Collections.sort(i.pueblos);
             System.out.print("|>>");
             for (int a = 0;a < i.nombre.length(); a++){
                 System.out.print(">");
@@ -80,20 +106,11 @@ public class ej22Manera2 {
             }
             System.out.println("<<|");
 
-            //Cada 30 caracteres se salta de línea
-            int cont = 0;
-            // En caso de que no esté vacío
-            if (!i.pueblos.isEmpty()) System.out.print("| ");
-            for (int j = 0; j < i.pueblos.size(); j++){
-                cont += i.pueblos.get(j).length();
-                if (cont >= 30){
-                    System.out.print("\n| ");
-                    cont = 0;
-                }
-                System.out.print(j+1 + " - " + i.pueblos.get(j) + " | ");
-            }
-            System.out.println("\n");
+            mostrarPueblosDeProvincia(i.pueblos, 0);
         }
+        System.out.print("Enter para continuar...");
+        sc.nextLine();
+        System.out.println();
     }
 
     static int transformarAEntero(String op){
@@ -114,12 +131,129 @@ public class ej22Manera2 {
         return pos;
          */
         try {
-            Integer pos = Integer.parseInt(op);
+            int pos = Integer.parseInt(op);
             return pos-1;
         } catch (Exception e){
-            return -1;
+            return -11;
         }
     }
 
+    static void mostrarPueblosDeProvincia(ArrayList<String> pueblos, int pausa){
+        System.out.println();
+        Collections.sort(pueblos);
+        int cont = 0;
+        if (!pueblos.isEmpty()) System.out.print("| ");
+        for (int j = 0; j < pueblos.size(); j++){
+            cont += pueblos.get(j).length();
+            if (cont >= 30){
+                System.out.print("\n| ");
+                cont = 0;
+            }
+            System.out.print(j+1 + " - " + pueblos.get(j) + " | ");
+        }
+        if (!pueblos.isEmpty())System.out.println("\n");
+        if (pausa == 1){
+            System.out.print("Enter para continuar...");
+            sc.nextLine();
+            System.out.println();
+        }
+    }
 
+    static void anadirProvincia(){
+        System.out.print("- Nombre: ");
+        String nombre = sc.nextLine();
+        ArrayList<String> nombreProvincias = new ArrayList<>();
+        for (provincias i : provinciasArray){
+            nombreProvincias.add(i.nombre);
+        }
+        if (nombreProvincias.contains(nombre)){
+            System.out.println("ERROR - El nombre ya existe");
+        } else {
+            provinciasArray.add(new provincias(nombre));
+            System.out.println("- Provincia \"" + nombre + "\" añadida");
+        }
+    }
+
+    static void eliminarProvincia(){
+        System.out.print("- Nombre: ");
+        String nombre = sc.nextLine();
+        int pos = -1;
+        for (int i = 0; i < provinciasArray.size(); i++ ){
+            if (provinciasArray.get(i).nombre.equals(nombre)){
+                pos = i;
+                break;
+            }
+        }
+        if (pos == -1) {
+            System.out.println("ERROR - El nombre no existe");
+        } else {
+            provinciasArray.remove(pos);
+            System.out.println("- Provincia \"" + nombre + "\" eliminada");
+        }
+    }
+
+    static void anadirPueblo(ArrayList<String> pueblos){
+        System.out.println("<------------------------------------------------------>");
+        do {
+            System.out.print("0 - para salir\n> Nombre: ");
+            String nombre = sc.nextLine();
+            if (nombre.equals("0")){
+                return;
+            } else if (pueblos.contains(nombre)){
+                System.out.println("ERROR - El nombre ya existe");
+            } else {
+                pueblos.add(nombre);
+                System.out.println("- Pueblo \"" + nombre + "\" añadido");
+            }
+        }while (true);
+
+    }
+
+    static void buscarPueblo(ArrayList<String> pueblos){
+        do {
+            Collections.sort(pueblos);
+            System.out.println("<------------------------------------------------------>");
+            System.out.print("0 - para salir\n> Nombre: ");
+            String nombre = sc.nextLine();
+            if (nombre.equals("0")){
+                return;
+            } else if (pueblos.contains(nombre)){
+                System.out.println("Encontrado en la posición " + (1 + Collections.binarySearch(pueblos, nombre)) );
+            } else {
+                do {
+                    System.out.print("No se a encontrado el elemento, ¿Quieres añadirlo? (s/n)\n> ");
+                    String op = sc.nextLine();
+                    if (op.equalsIgnoreCase("s")){
+                        pueblos.add(nombre);
+                        break;
+                    } else if (op.equalsIgnoreCase("n")){
+                        break;
+                    } else {
+                        System.out.println("ERROR - Opción inválida");
+                    }
+                }while (true);
+            }
+        }while (true);
+    }
+
+    static void eliminarPueblo(ArrayList<String> pueblos){
+        do {
+            System.out.println("<------------------------------------------------------>");
+            mostrarPueblosDeProvincia(pueblos, 0);
+            System.out.print("0 - para salir\n> Posición: ");
+            try {
+                int pos = Integer.parseInt(sc.nextLine());
+                if (pos == 0){
+                    return;
+                } else if (pos < 1 || pos > pueblos.size()){
+                    System.out.println("ERROR - Opción invalida");
+                } else {
+                    pueblos.remove(pos-1);
+                    System.out.println("- Pueblo \"" + pos + "\" eliminado");
+                }
+            } catch (Exception e){
+                System.out.println("ERROR - Carácter invalido");
+            }
+        }while (true);
+    }
 }
