83834d0e38dc781623f305c9fe50f7986d3b874d
Alonso <alonsoriano2007@gmail.com>
2026-01-16 17:30:21 +0100

    avance rule

--- Java/Medio/Practica/Demo.java
+++ Java/Medio/Practica/Demo.java
@@ -4,6 +4,6 @@ public class Demo {
     public static void main(String[] args) throws InterruptedException {
         Slime enemigo1 = new Slime("Slime1", 50, 50, 12, 0);
         Slime enemigo2 = new Slime(50, 50, 12, 0);
-        Personaje personaje = new Personaje("Hecproll", 100, 20, 0);
+        //Personaje personaje = new Personaje("Hecproll", 100, 20, 0);
     }
 }
