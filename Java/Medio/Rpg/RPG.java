474a428a9a53c47f6b8ec81f522db5da7d800da2
Alonso <asorianom2007@gmail.com>
2026-01-28 18:21:48 +0100

    breva13

--- Java/Medio/Rpg/RPG.java
+++ Java/Medio/Rpg/RPG.java
@@ -2,14 +2,32 @@ package Java.Medio.Rpg;
 
 import Java.Medio.Rpg.Cartas.Ataque.Corte;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class RPG {
     public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
+        impInicio();
         System.out.println("Introduce las acciónes por la linea de comandos");
         System.out.print("└─> ");
         sc.nextLine();
     }
 
+    static void impInicio(){
+        try (BufferedWriter escribir = new BufferedWriter(new FileWriter("Java/Medio/Rpg/Pantalla.txt"))){
+            String inicio = """
+                    ╔════════════════╗
+                    ║ Presiona ENTER ║
+                    ╚════════════════╝
+                    """;
+            escribir.write(inicio);
+        }catch (IOException e){
+            System.out.println("Pantalla no encontrada");
+        }
+    }
 }
