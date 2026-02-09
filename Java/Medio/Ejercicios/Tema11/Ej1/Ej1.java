8fd83cab4184b4b80bb5793649c3fe37a32c448e
Alonso <asorianom2007@gmail.com>
2026-02-09 21:18:20 +0100

    breva19abc

--- Java/Medio/Ejercicios/Tema11/Ej1/Ej1.java
+++ Java/Medio/Ejercicios/Tema11/Ej1/Ej1.java
@@ -1,30 +1,45 @@
 package Java.Medio.Ejercicios.Tema11.Ej1;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class Ej1 {
-    static List<String> registros = new ArrayList<>();
+    static List<Registro> registros = new ArrayList<>();
     public static void main(String[] args) {
 
         try (BufferedReader leer = new BufferedReader( new FileReader("Java/Medio/Ejercicios/Tema11/Ej1/DATA.csv"))){
             String linea;
+            boolean primera=true;
             while ((linea = leer.readLine()) != null){
-                registros.add(linea);
+                if (!primera){
+                     String[] registro = linea.split(",");
+                     registros.add(new Registro(Integer.parseInt(registro[0]), registro[1], registro[2], Double.parseDouble(registro[3]), Double.parseDouble(registro[4]), registro[5], registro[6], registro[7], registro[8], LocalDateTime.parse(registro[9].replace(" ", "T"))));
+                } else {
+                    primera=false;
+                }
             }
         } catch (IOException e){
             System.out.println("Archivo no encontrado");
         }
-        registros.removeFirst();
+      //  registros.forEach(System.out::println);
         System.out.println("1- Los registros del pahís FR");
-        // Esto elimina todos los que no sean FR pero modifica la lista
-//        registros.removeIf(registros -> !registros.contains(",FR,"));
-        // Aplica un filtro que busca ",FR," y muestra el resultado
-        registros.stream().filter(registros -> registros.contains(",FR,")).forEach(System.out::println);
+        registros.stream().filter(registro -> registro.getPais().equals("FR")).forEach(System.out::println);
+
         System.out.println("<==========================>\n<==========================>\n<==========================>\n<==========================>\n<==========================>");
         System.out.println("2- Los registros con email de yahoo ordenados por nombre");
-        registros.stream().filter(registros -> registros.contains("@yahoo")).sorted().forEach(System.out::println);
+        registros.stream().filter(registro -> registro.getEmail().contains("@yahoo")).sorted(Comparator.comparing(Registro::getNombre)).forEach(System.out::println);
+
+        System.out.println("<==========================>\n<==========================>\n<==========================>\n<==========================>\n<==========================>");
+        System.out.println("3- Los registros con el último login entre noviembre de 2015 y enero de 2017 ambos incluidos ordenados por fecha de manera descendente.");
+        registros.stream().filter(registro -> registro.getLast_login_date().isAfter(LocalDateTime.parse("2015-11-01T00:00:00")) && registro.getLast_login_date().isBefore(LocalDateTime.parse("2017-01-01T00:00:00"))).forEach(System.out::println);
+
+        System.out.println("<==========================>\n<==========================>\n<==========================>\n<==========================>\n<==========================>");
+        System.out.println("3- Los nombres y apellidos de los registros cuyo nombre (first_name) empiecen por ‘A’ ordenados por fecha (descendente) del último inicio de sesión (last_login).");
+        registros.stream().filter(registro -> registro.getNombre().startsWith("A")).sorted(Comparator.comparing(Registro::getLast_login_date)).forEach(System.out::println);
     }
 }
---
+++ Java/Medio/Ejercicios/Tema11/Ej1/Registro.java
@@ -0,0 +1,89 @@
+package Java.Medio.Ejercicios.Tema11.Ej1;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Registro {
+    //id,first_name,last_name,latitude,longitude,domain,email,country,ip_address,last_login
+    int id;
+    String nombre;
+    String apellido;
+    double latitud;
+    double longitud;
+    String dominio;
+    String email;
+    String pais;
+    String direccion_ip;
+    LocalDateTime last_login_date;
+
+
+    public Registro(int id, String nombre, String apellido, double latitud, double longitud, String dominio, String email, String pais, String direccion_ip, LocalDateTime last_login_date) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.dominio = dominio;
+        this.email = email;
+        this.pais = pais;
+        this.direccion_ip = direccion_ip;
+        this.last_login_date = last_login_date;
+    }
+
+    @Override
+    public String toString() {
+        return "Registro{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", latitud=" + latitud +
+                ", longitud=" + longitud +
+                ", dominio='" + dominio + '\'' +
+                ", email='" + email + '\'' +
+                ", pais='" + pais + '\'' +
+                ", direccion_ip='" + direccion_ip + '\'' +
+                ", last_login_date=" + last_login_date +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public String getDominio() {
+        return dominio;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public String getDireccion_ip() {
+        return direccion_ip;
+    }
+
+    public LocalDateTime getLast_login_date() {
+        return last_login_date;
+    }
+}
