ef327b53d3720e43d2e4243be86aeb0bf2514d08
Alonso <alonsoriano2007@gmail.com>
2026-04-11 13:57:21 +0200

    Locura2

--- Proyecto/BBDD/DinopolisBBDD.sql
+++ Proyecto/BBDD/DinopolisBBDD.sql
@@ -134,3 +134,148 @@ create table Pertenece(
 	constraint fk_numeroDeAtraccion_Pertenece foreign key (numeroDeAtraccion) references Atracciones (numeroDeAtraccion),
 	constraint fk_nombre_Pertenece foreign key (nombre) references Categoria (nombre)
 );
+
+insert into Persona ()
+	values
+	('11111111A', 23, 'Juan Carlos'),
+	('22222222A', 73, 'Maria'),
+	('33333333A', 34, 'Carlos Casado'),
+	
+	('44444444A', 12, 'Marcos Inojosa'),
+	('55555555A', 43, 'Emanuel Ciro'),
+	('66666666A', 82, 'Pablo Miguel'),
+	('77777777A', 45, 'Carla Romero'),
+	('88888888A', 34, 'Iker Lozano'),
+	('99999999A', 23, 'Hernesto'),
+	('11111111B', 53, 'Hector Criado'),
+	('22222222B', 19, 'Sara'),
+	('33333333B', 64, 'Jeffry'),
+	('44444444B', 53, 'Estela Ramirez');
+
+insert into Clientes ()
+	values
+	('11111111A'),
+	('22222222A'),
+	('33333333A');
+
+insert into Entrada (tipo, precio, DNI)
+	values 
+	('Oferta', 15.26, '11111111A'),
+	('Normal', 21.58, '11111111A'),
+	('Familia numerosa', 17.20, '22222222A');
+
+insert into Trabajadores ()
+	values
+	('44444444A', '11 12345678 90', '44444444A'),
+	('55555555A', '22 12345678 90', '44444444A'),
+	('66666666A', '33 12345678 90', '44444444A'),
+	('77777777A', '44 12345678 90', '77777777A'),
+	('88888888A', '55 12345678 90', '77777777A'),
+	('99999999A', '66 12345678 90', '99999999A'),
+	('11111111B', '77 12345678 90', '99999999A'),
+	('22222222B', '88 12345678 90', '22222222B'),
+	('33333333B', '99 12345678 90', '22222222B'),
+	('44444444B', '00 12345678 90', '22222222B');
+
+insert into Vigilantes ()
+	values
+	('44444444A'),
+	('55555555A'),
+	('66666666A');
+
+insert into Limpiadores ()
+	values
+	('77777777A'),
+	('88888888A');
+
+insert into Cajeros ()
+	values
+	('99999999A'),
+	('11111111B');
+
+insert into Actores ()
+	values
+	('22222222B'),
+	('33333333B'),
+	('44444444B');
+
+
+insert into Zonas (nombre)
+	values
+	('Paleosenda'),
+	('Entrada'),
+	('Patio exterior');
+
+insert into Visita ()
+	values 
+	('11111111A', 2),
+	('33333333A', 2),
+	('11111111A', 3),
+	('33333333A', 3),
+	('22222222A', 2),
+	('22222222A', 3);
+
+insert into ZonasEntretenimiento ()
+	values 
+	(1),
+	(3);
+
+insert into ZonasRecepcion ()
+	values 
+	(2);
+
+insert into Vigila ()
+	values 
+	(2, '44444444A'),
+	(1, '55555555A'),
+	(3, '66666666A');
+
+insert into Limpia ()
+	values 
+	(1, '77777777A'),
+	(2, '77777777A'),
+	(3, '88888888A');
+
+insert into Trabaja ()
+	values 
+	(2, '99999999A'),
+	(2, '11111111B');
+
+insert into Atracciones (nombre)
+	values 
+	('Laberinto'),
+	('Museo'),
+	('Trenecito'),
+	('Teatro');
+
+insert into AtraccionesZona ()
+	values 
+	(1, 1),
+	(2, 3),
+	(3, 3),
+	(4, 3);
+
+insert into Actua ()
+	values 
+	(4, '22222222B'),
+	(4, '33333333B'),
+	(4, '44444444B');
+
+insert into Categoria ()
+	values 
+	('Informativo', 'Contiene información interesante'),
+	('Para niños', 'Enfocado a un publico infantil'),
+	('Colaborativo', 'Requiere de la colaboración del publico'),
+	('Claustrofobico', 'Puede ser peligroso para personas claustrofobicas');
+
+insert into Pertenece ()
+	values 
+	(1, 'Claustrofobico'),
+	(1, 'Para niños'),
+	(2, 'Informativo'),
+	(3, 'Para niños'),
+	(4, 'Para niños'),
+	(4, 'Informativo'),
+	(4, 'Colaborativo');
+
+
