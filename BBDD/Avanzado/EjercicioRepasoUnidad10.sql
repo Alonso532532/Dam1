cb06d7d022fd4db8039616670364a03f6143ea99
Alonso <asorianom2007@gmail.com>
2026-05-07 15:48:56 +0200

    cosas

---
+++ BBDD/Avanzado/EjercicioRepasoUnidad10.sql
@@ -0,0 +1,30 @@
+-- 1
+CALL ejercicio1(20);
+
+-- 2
+call ejercicio2(30, 20);
+
+-- 3
+call ejercicio3("Europe", "Spanish");
+
+-- 4
+select ejercicio4(20);
+
+-- 5
+set @palabra="Hola hecprooll";
+call ejercicio5(@palabra);
+select @palabra;
+
+-- 6
+call ejercicio6(1, "PRUEBA");
+
+-- 7
+select ejercicio7("PENELOPE", "GUINESS");
+
+-- 8
+call ejercicio8("New");
+
+-- 9
+
+
+
