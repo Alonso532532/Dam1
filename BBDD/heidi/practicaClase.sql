65cfa9f3168503b1ab3b26613856f51d98bc52ca
Alonso <asorianom2007@gmail.com>
2026-05-11 15:48:25 +0200

    cosas

--- BBDD/heidi/practicaClase.sql
+++ BBDD/heidi/practicaClase.sql
@@ -1,5 +1,9 @@
 with totalEmp as
-	(select id_empleado, SUM(precio) as "promedio" from cita group by id_empleado)
+	(select id_empleado, SUM(precio) as "total" from cita group by id_empleado)
 	select e.nombre from empleado e where 
-	(select t.promedio from totalEmp t where t.id_empleado = e.id) > 
-	(select AVG(t.promedio) from totalEmp t);
+	(select t.total from totalEmp t where t.id_empleado = e.id) > 
+	(select AVG(t.total) from totalEmp t);
+
+with totalEmp as
+	(select id_empleado, SUM(precio) as "total" from cita group by id_empleado)
+	select t.total from totalEmp t union select AVG(t.total) from totalEmp t;
